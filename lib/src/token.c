6ee71c31ab26cddd51d84924c7592ebc34481598
ketchupcomponent <101750090+iHategithub9000@users.noreply.github.com>
2025-11-13 16:19:40 +0100

    Update token.c to include Windows tokens

--- lib/src/token.c
+++ lib/src/token.c
@@ -8,13 +8,13 @@ const char *iuab_token_type_name(enum iuab_token_type type) {
     case IUAB_TOKEN_EOF: return "EOF";
     case IUAB_TOKEN_I: return "i";
     case IUAB_TOKEN_USE: return "use";
-    case IUAB_TOKEN_ARCH: return "arch";
-    case IUAB_TOKEN_LINUX: return "linux";
+    case IUAB_TOKEN_WINDOWS: return "windows";
+    case IUAB_TOKEN_TEN: return "10";
     case IUAB_TOKEN_BTW: return "btw";
     case IUAB_TOKEN_BY: return "by";
     case IUAB_TOKEN_THE: return "the";
     case IUAB_TOKEN_WAY: return "way";
-    case IUAB_TOKEN_GENTOO: return "gentoo";
+    case IUAB_TOKEN_WINDOWSME: return "windowsME";
     default: return "???";
     }
 }
